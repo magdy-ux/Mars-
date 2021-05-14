9cf7a27a854d50fbc3aec74a5e19d18f9c9ac960
Noureldin7 <83598821+Noureldin7@users.noreply.github.com>
2021-05-14 21:45:27 +0200

    Update EventF.h

--- EventF.h
+++ EventF.h
@@ -1,5 +1,11 @@
 #pragma once
 #include "Event.h"
+#include "Mission.h"
 class EventF :public Event
 {
+private:
+	int TYP; int ED; int ID; int TLOC; int MDUR; int SIG;
+public:
+	EventF(int TYP,int ED,int ID,int TLOC,int MDUR,int SIG);
+	void Execute();
 };
