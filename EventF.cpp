87de858e29672bba4db330577a83143f6fa3ff9f
Noureldin7 <83598821+Noureldin7@users.noreply.github.com>
2021-05-14 21:41:58 +0200

    Update EventF.cpp

--- EventF.cpp
+++ EventF.cpp
@@ -1,1 +1,10 @@
 #include "EventF.h"
+#include "Mission.h"
+EventF::EventF(int a,int b,int c,int d,int e,int f)
+{
+	TYP = a; ED = b; ID = c;  TLOC = d;  MDUR = e;  SIG = f;;
+}
+void EventF::Execute()
+{
+
+}
