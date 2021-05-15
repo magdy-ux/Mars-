60a1d8d993dab3cb4a6eb2a4dae6712406f01c4f
Noureldin7 <83598821+Noureldin7@users.noreply.github.com>
2021-05-15 14:08:29 +0200

    Update EventF.cpp

--- EventF.cpp
+++ EventF.cpp
@@ -2,9 +2,17 @@
 #include "Mission.h"
 EventF::EventF(int a,int b,int c,int d,int e,int f)
 {
-	TYP = a; ED = b; ID = c;  TLOC = d;  MDUR = e;  SIG = f;;
+	TYP = a; ED = b; ID = c;  TLOC = d;  MDUR = e;  SIG = f; M =nullptr;
 }
 void EventF::Execute()
 {
-
+	M=new Mission(TYP, ID, TLOC, MDUR, SIG);
+}
+Mission* EventF::getMission()
+{
+	return M;
+}
+int EventF::getday()
+{
+	return ED;
 }
