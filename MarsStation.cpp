42c0fb5999a7a7c39ad471d1f4c3be5a9b923ba9
Noureldin7 <83598821+Noureldin7@users.noreply.github.com>
2021-05-15 14:11:59 +0200

    Update MarsStation.cpp

--- MarsStation.cpp
+++ MarsStation.cpp
@@ -1,1 +1,35 @@
 #include "MarsStation.h"
+#include "Event.h"
+#include "EventF.h"
+#include "EventP.h"
+#include "EventC.h"
+MarsStation::MarsStation()
+{
+
+}
+void MarsStation::StationInput(LinkedQueue<Event>*)
+{
+
+}
+void MarsStation::MExecute()
+{
+	while (true)//dummy condition//
+	{
+		Event *E;
+		if (dynamic_cast<EventF*>(E))
+		{
+			EventF Ef;
+			bool b = EventList.peek(Ef);
+			if (b && CurrentDay == Ef.getday())
+			{
+				EventList.dequeue(Ef);
+				Ef.Execute();
+				Mission* M = Ef.getMission();
+				if (M->gettype() == 3)
+					EMissionList.InsertEnd(*M);//priority needed
+				else
+					MPMissionList.InsertEnd(*M);
+			}
+		}
+	}
+}
