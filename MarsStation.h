790ad9a46248a38d3ef5345df967d6c033ff3954
Noureldin7 <83598821+Noureldin7@users.noreply.github.com>
2021-05-15 14:05:49 +0200

    Update MarsStation.h

--- MarsStation.h
+++ MarsStation.h
@@ -1,12 +1,23 @@
+#pragma once
+#include "linkedqueue.h"
+#include "Linkedlist.h"
+#include "Event.h"
 #include "Mission.h"
+#include"Rover.h"
+#include "UI.h"
 class MarsStation
 {
 private:
 	int CurrentDay;
+	UI UserI;
 	LinkedQueue<Event> EventList;
-	LinkedQueue<Mission> MPMissionList;
-	LinkedQueue<Mission> EMissionList;
+	LinkedList<Mission> MPMissionList;
+	LinkedList<Mission> EMissionList;
+	LinkedQueue<Rover> RoverList;
 public:
+	MarsStation();
+	void StationInput(LinkedQueue<Event>*);
+	void MExecute();
 
 };
 
