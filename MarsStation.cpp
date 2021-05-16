b4f2a6370ac21a8ba7c8bb1b3c8d122b9ba22b01
Noureldin7 <83598821+Noureldin7@users.noreply.github.com>
2021-05-16 15:14:56 +0200

    Update MarsStation.cpp

--- MarsStation.cpp
+++ MarsStation.cpp
@@ -1,8 +1,7 @@
 #include "MarsStation.h"
+#include "Mission.h"
 #include "Event.h"
 #include "EventF.h"
-#include "EventP.h"
-#include "EventC.h"
 MarsStation::MarsStation()
 {
 
@@ -13,23 +12,38 @@ void MarsStation::StationInput(LinkedQueue<Event>*)
 }
 void MarsStation::MExecute()
 {
-	while (true)//dummy condition//
+	EventF E;
+	bool b = EventList.peek(E);
+	if (b && CurrentDay == E.getday())
 	{
-		Event *E;
-		if (dynamic_cast<EventF*>(E))
+		EventList.dequeue(E);
+		E.Execute();
+		Mission* M = E.getMission();
+		if (M->gettype() == 3)
+			EMissionList.InsertEnd(*M);//priority needed
+		else
+			PMissionList.InsertEnd(*M);
+	}
+}
+void MarsStation::Assign()
+{
+	if (EMissionList.peek())
+	{
+		Mission* M = EMissionList.peek();
+		if (M->gettype() == 1)//Polar
 		{
-			EventF Ef;
-			bool b = EventList.peek(Ef);
-			if (b && CurrentDay == Ef.getday())
+			Rover R;
+			if (!PRoverList.isEmpty())
 			{
-				EventList.dequeue(Ef);
-				Ef.Execute();
-				Mission* M = Ef.getMission();
-				if (M->gettype() == 3)
-					EMissionList.InsertEnd(*M);//priority needed
-				else
-					MPMissionList.InsertEnd(*M);
+				PRoverList.dequeue(R);
+				InExRovers.enqueue(R);
+				PMissionList.dequeue(R);
+				InExMissions.enqueue(R);
 			}
+		}
+		if (M->gettype() == 2)//Emergency
+		{
+
 		}
 	}
 }
