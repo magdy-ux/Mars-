5340eb2b57828ed5096aa8983c84cad51c5858f6
magdy-ux <62838318+magdy-ux@users.noreply.github.com>
2021-05-17 15:07:00 +0200

    Create Defs.h

---
+++ Defs.h
@@ -0,0 +1,19 @@
+#pragma once
+enum missontype
+{
+	polar,
+	mountainous,
+	emergency
+
+};
+enum rovertype
+{
+	polarrover,
+	mountainousrover,
+	emergencyrover
+
+};
+enum eventtype
+{
+	formulation,
+};
