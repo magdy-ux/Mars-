834d1df89c63c731a040f81a1495ac6eecb79bad
magdy-ux <62838318+magdy-ux@users.noreply.github.com>
2021-05-17 15:11:45 +0200

    Update Rover.h

--- Rover.h
+++ Rover.h
@@ -1,5 +1,16 @@
 #pragma once
+#include"Defs.h"
 class Rover
 {
+  virtual int getid()=0;
+	virtual void setid(int x) =0 ;
+	virtual void setspeed(int)=0;
+	virtual int getspeed()=0;
+	virtual int getcheckupdayes()=0;
+	virtual void setcheckupdayes(int c)=0;
+	virtual rovertype gettype() = 0;
+	virtual void settype(rovertype r) = 0;
+	virtual int getwattingdays()=0;
+	virtual void setwaittingdays(int c)=0;
 };
 
